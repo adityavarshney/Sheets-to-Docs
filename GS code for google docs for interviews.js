8830ab38847a1e12d16c19897bdfe3ef78affc1a
Aditya Varshney <adityavarshney@gmail.com>
2018-02-01 00:47:22 -0800

    Uploaded interview doc generator (GS)

---
+++ GS code for google docs for interviews.js
@@ -0,0 +1,30 @@
+function createDocs() {
+  // trim function for String class
+  if(typeof(String.prototype.trim) === "undefined")
+  {
+    String.prototype.trim = function() 
+    {
+      return String(this).replace(/^\s+|\s+$/g, '');
+    };
+  }
+  
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheets()[0];
+  var data = sheet.getDataRange().getValues();
+  var yearAndSem = Browser.inputBox("Insert current year followed by semester: (Spring 2017, Fall 2018, Fall 2052, etc.)").trim();
+  var dir = DriveApp.createFolder("Interviews " + yearAndSem);
+  
+  for (var i = Math.min(2, data.length); i <= data.length; i++) {
+    var candidateName = sheet.getRange(i, 1).getValues()[0][0].trim();
+    var doc = DocumentApp.create(candidateName + " interview profile " + yearAndSem);
+    var body = doc.getBody();
+    for (var j = Math.min(0, data[i].length); j < data[i].length; j++) {
+      body.appendParagraph(data[0][j]);
+      body.appendParagraph(data[i - 1][j]);
+      body.appendParagraph("");
+    }
+    var file = DriveApp.getFileById(doc.getId());
+    dir.addFile(file);
+    DriveApp.removeFile(file);
+  }
+}
